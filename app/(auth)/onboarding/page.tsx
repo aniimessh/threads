43e8755d93719c0240048caf56526893bbf1827c
aniimessh <animeshgupta2.002@gmail.com>
2024-08-13 16:06:27 +0530

    fix : onbaording page fixed

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,5 +1,7 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 interface UserInfo {
   _id?: string;
@@ -11,14 +13,16 @@ interface UserInfo {
 
 async function Page() {
   const user = await currentUser();
-  const userInfo: UserInfo = {};
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
   const userData = {
     id: user?.id ?? "",
-    objectId: userInfo._id ?? "",
-    username: userInfo.username ?? user?.username ?? "",
-    name: userInfo.name ?? user?.firstName ?? "",
-    bio: userInfo.bio ?? "",
-    image: userInfo.image ?? user?.imageUrl ?? "",
+    objectId: userInfo ? userInfo?._id : "",
+    username: userInfo ? userInfo.username : user?.username,
+    name: userInfo ? userInfo.name : user?.firstName,
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user?.imageUrl,
   };
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
--- app/(root)/communites/page.tsx
+++ app/(root)/communites/page.tsx
@@ -1,9 +1,57 @@
-import React from "react";
+import CommunityCard from "@/components/cards/CommunityCard";
+import UserCard from "@/components/cards/UserCard";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
+import { fetchCommunities } from "@/lib/actions/community.action";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+const Page = async () => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  const userInfoPlain = userInfo.toJSON();
+  const userId = userInfoPlain._id.toString();
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchCommunities({
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
   return (
     <section className="">
-      <h1 className="head-text">Community Page</h1>
+      <h1 className="head-text">
+        {/* search bar */}
+        <div className="mt-14 flex flex-col gap-9">
+          {result && result.communities.length === 0 ? (
+            <p className="no-result">No communities found</p>
+          ) : (
+            <>
+              {result &&
+                result.communities.map((community) => (
+                  <CommunityCard
+                    key={community.id}
+                    name={community.name}
+                    username={community.username}
+                    id={community.id}
+                    imgUrl={community.image}
+                    bio={community.bio}
+                    members={community.members}
+                  />
+                ))}
+            </>
+          )}
+        </div>
+      </h1>
     </section>
   );
 };
