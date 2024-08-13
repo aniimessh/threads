f68040ea9fb9dbc5f6c2c6802f4f1d720cc0ba9c
aniimessh <animeshgupta2.002@gmail.com>
2024-08-13 10:24:51 +0530

    fix : profile page route fixed

--- components/shared/Leftsidebar.tsx
+++ components/shared/Leftsidebar.tsx
@@ -4,11 +4,12 @@ import Link from "next/link";
 import { sidebarLinks } from "../../constants/index";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import { SignedIn, SignOutButton, useAuth } from "@clerk/nextjs";
 
 const Leftsidebar = () => {
   const router = useRouter();
   const pathname = usePathname();
+  const { userId } = useAuth();
   return (
     <section className="leftsidebar custom-scrollbar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
@@ -16,6 +17,7 @@ const Leftsidebar = () => {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
           return (
             <Link
               href={link.route}
