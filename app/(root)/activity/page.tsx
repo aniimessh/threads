82234cf2587bc821fc866c577659d32d918150ef
aniimessh <animeshgupta2.002@gmail.com>
2024-08-13 11:09:18 +0530

    feat : activity page ⭐

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,9 +1,51 @@
-const Page = () => {
-    return (
-      <section className="">
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  const userInfoPlain = userInfo.toJSON();
+  const userId = userInfoPlain._id.toString();
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const activity = await getActivity(userInfo._id);
+  return (
+    <section className="">
       <h1 className="head-text">Acitivity Page</h1>
+      <section className="mt-10 flex flex-col gap-5">
+        {activity && activity.length > 0 ? (
+          <>
+            {activity?.map((activity) => (
+              <Link href={`/thread/${activity.id}`} key={activity.id}>
+                <article className="activity-card">
+                  <Image
+                    src={activity.author.image}
+                    alt="profile-image"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">{activity.author.name}</span>
+                    replied to your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="text-light-1 !text-base-regular">No Activity Found</p>
+        )}
       </section>
-    )
-  }
-  
-  export default Page
+    </section>
+  );
+};
+
+export default Page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -7,118 +7,140 @@ import Thread from "../models/thread.model";
 import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params {
-    userId: string,
-    username: string,
-    name: string,
-    bio: string,
-    image: string,
-    path: string
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
 }
 
 export async function updateUser({
-    userId,
-    username,
-    name,
-    bio,
-    image,
-    path,
+  userId,
+  username,
+  name,
+  bio,
+  image,
+  path,
 }: Params): Promise<void> {
-    try {
-        connectToDB();
+  try {
+    connectToDB();
 
-        const result = await User.findOneAndUpdate(
-            { id: userId },
-            { username: username.toLowerCase(), name, image, bio, onboarded: true },
-            { upsert: true }
-        );
-        console.log("result", result)
-        if (path === "/profile/edit") {
-            revalidatePath(path);
-        }
-    } catch (err: any) {
-        throw new Error(`Failed to create/update user ${err.message}`);
+    const result = await User.findOneAndUpdate(
+      { id: userId },
+      { username: username.toLowerCase(), name, image, bio, onboarded: true },
+      { upsert: true }
+    );
+    console.log("result", result);
+    if (path === "/profile/edit") {
+      revalidatePath(path);
     }
+  } catch (err: any) {
+    throw new Error(`Failed to create/update user ${err.message}`);
+  }
 }
 
 export async function fetchUser(userId: string) {
-    try {
-        connectToDB();
-        return await User.findOne({ id: userId })
-        // .populate({
-        //     // path: "communites",
-        //     // model: "Community"
-        // });
-    } catch (err: any) {
-        throw new Error(`Failed to fetch user ${err.message}`)
-    }
+  try {
+    connectToDB();
+    return await User.findOne({ id: userId });
+    // .populate({
+    //     // path: "communites",
+    //     // model: "Community"
+    // });
+  } catch (err: any) {
+    throw new Error(`Failed to fetch user ${err.message}`);
+  }
 }
 
 export async function fetchUserPost(userId: string) {
-    try {
-        connectToDB();
-        const threads = await User.findOne({ id: userId }).populate(
-            {
-                path: "threads",
-                model: Thread,
-                populate: {
-                    path: "children",
-                    model: Thread,
-                    populate: {
-                        path: "author",
-                        model: User,
-                        select: "name image id"
-                    }
-                }
-            },
-        )
-        return threads
-    } catch (error: any) {
-        console.log(`Error while fetching user post: ${error.message}`);
-    }
+  try {
+    connectToDB();
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+    return threads;
+  } catch (error: any) {
+    console.log(`Error while fetching user post: ${error.message}`);
+  }
 }
 
 export async function fetchUsers({
-    userId,
-    searchString = "",
-    pageNumber = 1,
-    pageSize = 20,
-    sortBy = "desc"
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
 }: {
-    userId: string,
-    searchString?: string,
-    pageNumber?: number,
-    pageSize?: number,
-    sortBy?: SortOrder
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
 }) {
-    try {
-        connectToDB();
-        const skipAmount = (pageNumber - 1) * pageSize;
-        const regex = new RegExp(searchString, "i");
-        const query: FilterQuery<typeof User> = {
-            id: { $ne: userId }
-        }
+  try {
+    connectToDB();
+    const skipAmount = (pageNumber - 1) * pageSize;
+    const regex = new RegExp(searchString, "i");
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId },
+    };
 
-        if (searchString.trim() !== "") {
-            query.$or = [
-                { username: { $regex: regex } },
-                { name: { $regex: regex } }
-            ]
-        }
-
-        const sortOptions = { createdAt: sortBy }
-
-        const userQuery = User.find(query)
-            .sort(sortOptions)
-            .skip(skipAmount)
-            .limit(pageSize);
-
-        const totalPostCount = await User.countDocuments(query);
-        const users = await userQuery.exec();
-
-        const isNext = totalPostCount > skipAmount + users.length;
-
-        return { users, isNext };
-    } catch (err: any) {
-        console.log(`Error while fetching users: ${err.message}`);
+    if (searchString.trim() !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
     }
+
+    const sortOptions = { createdAt: sortBy };
+
+    const userQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalPostCount = await User.countDocuments(query);
+    const users = await userQuery.exec();
+
+    const isNext = totalPostCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (err: any) {
+    console.log(`Error while fetching users: ${err.message}`);
+  }
+}
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+    const userThreads = await Thread.find({ author: userId });
+
+    const childThreadIds = userThreads.reduce((acc, thread) => {
+      return acc.concat(thread.children);
+    }, []);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (err: any) {
+    console.log(`Error while geting acitiy: ${err.message}`);
+  }
 }
